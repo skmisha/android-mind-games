1448bc7f5792ad0580e0672d6a0fccdebbb80ca5
skmisha <mkushniriov@gmail.com>
2013-02-11 23:18:06 +0200

    added Seria class

---
+++ src/com/umind/games/Seria.java
@@ -0,0 +1,21 @@
+package com.umind.games;
+
+public class Seria {
+	private String description;
+	private int firstElement;
+	private int lastElement;
+	private int step;
+	
+	public int getStep(){
+		return step;
+	}
+	public int getFirstElement(){
+		return firstElement;
+	}
+	public int getLastElement(){
+		return lastElement;
+	}
+	public String getDescription(){
+		return description;
+	}
+}
--- src/com/umind/games/seriesGame.java
+++
@@ -1,5 +0,0 @@
-package com.umind.games;
-
-public class seriesGame {
-	public String description;
-}
