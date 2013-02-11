49c5bdce9d58fa631719b429448125bd6991843b
skmisha <mkushniriov@gmail.com>
2013-02-11 22:07:52 +0200

    first one

---
+++ src/com/umind/games/startPage.java
@@ -0,0 +1,13 @@
+package com.umind.games;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class startPage extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
