b0dc83da1248244315c3a509e232d37c494655fd
skmisha <mkushniriov@gmail.com>
2013-02-12 00:28:38 +0200

    new folder

--- src/com/umind/games/Seria.java
+++ src/com/umind/games/Seria.java
@@ -5,7 +5,21 @@ public class Seria {
 	private int firstElement;
 	private int lastElement;
 	private int step;
+	public Seria(int f, int l, int s){
+		setFirstElement(f);
+		setLastElement(l);
+		setStep(s);
+	}
 	
+	public void setStep(int s){
+		step=s;
+	}
+	public void setLastElement(int l){
+		lastElement=l;
+	}
+	public void setFirstElement(int f){
+		firstElement=f;
+	}
 	public int getStep(){
 		return step;
 	}
