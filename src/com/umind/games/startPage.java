8d09d11fd7f30f6299dc0b0879e945d129ce8e84
skmisha <mkushniriov@gmail.com>
2013-02-12 22:19:12 +0200

    generated

--- src/com/umind/games/startPage.java
+++ src/com/umind/games/startPage.java
@@ -9,6 +9,5 @@ public class startPage extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
     }
 }
