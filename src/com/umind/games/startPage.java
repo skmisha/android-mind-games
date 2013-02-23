4bb180c0b65a88139ba83dcfddf50e2ea09bb8ee
skmisha <mkushniriov@gmail.com>
2013-02-23 18:42:21 +0200

    aa

--- src/com/umind/games/startPage.java
+++ src/com/umind/games/startPage.java
@@ -1,16 +1,36 @@
 package com.umind.games;
 
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class startPage extends Activity {
     /** Called when the activity is first created. */
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+    super.onCreate(savedInstanceState);    
+    setContentView(R.layout.main);
     }
     public Seria s = new Seria(0,100,3);
-    
-    
 }
