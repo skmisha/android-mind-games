1ebdb2445cedfecb04ecb4a22a24ec0a2f67d1d9
skmisha <mkushniriov@gmail.com>
2013-02-23 18:16:10 +0200

    added few functions to Seria and resized buttons

--- src/com/umind/games/Seria.java
+++ src/com/umind/games/Seria.java
@@ -5,12 +5,19 @@ public class Seria {
 	private int firstElement;
 	private int lastElement;
 	private int step;
+	private int currentElement;
+	private int index=0;
+	
 	public Seria(int f, int l, int s){
 		setFirstElement(f);
 		setLastElement(l);
 		setStep(s);
 	}
 	
+	public int getNextElement(){
+		index++;
+		return (currentElement+step);
+	}
 	public void setStep(int s){
 		step=s;
 	}
@@ -31,5 +38,11 @@ public class Seria {
 	}
 	public String getDescription(){
 		return description;
+	}
+	public void setCurrentElement(int c){
+		currentElement=c;
+	}
+	public int getCurrentElement(){
+		return currentElement;
 	}
 }
--- src/com/umind/games/startPage.java
+++ src/com/umind/games/startPage.java
@@ -12,4 +12,5 @@ public class startPage extends Activity {
     }
     public Seria s = new Seria(0,100,3);
     
+    
 }
